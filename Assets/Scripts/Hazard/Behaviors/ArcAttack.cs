3b55b36681eac9fef435bab68e62e22e9c9e3d0b
Ryan Mauldin <ryant.mauldin@gmail.com>
2022-10-26 17:55:26 -0700

    tried to interpolate the setup on the arc attack

--- Assets/Scripts/Hazard/Behaviors/ArcAttack.cs
+++ Assets/Scripts/Hazard/Behaviors/ArcAttack.cs
@@ -16,7 +16,7 @@ public class ArcAttack : BehaviorBase
         var adjustedAngleRadians = Mathf.Deg2Rad * (angle + 90 - ArcAngle/2);
         return origin + ArcRadius * new Vector2(Mathf.Cos(adjustedAngleRadians), Mathf.Sin(adjustedAngleRadians));
     }
-
+    
     private IEnumerator SpawnProjectiles(SequenceData data)
     {
         // var delta = 1.0f/data.ProjectileCount;
@@ -29,11 +29,25 @@ public class ArcAttack : BehaviorBase
             var angle = Mathf.Lerp(0, ArcAngle, delta);
             var point = CalculateArcPoint(data.From, angle);
             var projectileScript = obj.GetComponent<HazardProjectile>();
+
             var commands = new Queue<HazardCommand>();
-            commands.Enqueue(new MoveCommand(data.From, point, data.Speed * SetupSpeedMultiplier));
+            var pos1 = Vector2.MoveTowards(data.From, point, Vector2.Distance(data.From, point)/2);
+            var pos2 = Vector2.MoveTowards(pos1, point, Vector2.Distance(pos1, point)/2);
+            var pos3 = Vector2.MoveTowards(pos2, point, Vector2.Distance(pos2, point)/2);
+            var pos4 = Vector2.MoveTowards(pos3, point, Vector2.Distance(pos3, point));
+
+
+            // commands.Enqueue(new MoveCommand(data.From, point, data.Speed * SetupSpeedMultiplier));
+            commands.Enqueue(new MoveCommand(data.From, pos1, data.Speed * SetupSpeedMultiplier));
+            commands.Enqueue(new MoveCommand(data.From, pos2, data.Speed * SetupSpeedMultiplier * 0.5f));
+            commands.Enqueue(new MoveCommand(data.From, pos3, data.Speed * SetupSpeedMultiplier * 0.25f));
+            commands.Enqueue(new MoveCommand(data.From, pos4, data.Speed * SetupSpeedMultiplier * 0.125f));
+
             commands.Enqueue(new WaitCommand(data.SecondsBetweenProjectiles));
             commands.Enqueue(new WaitCommand(i * data.SecondsBetweenProjectiles));
+
             commands.Enqueue(new FireCommand(point, data.To, data.Speed));
+            
             obj.SetActive(true);
             if(obj == null) continue;
             projectileScript.ExecuteCommands(commands);
