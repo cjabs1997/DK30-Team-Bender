17c6b8e006e64a8c3e70212d11e20e920a9ea565
Ryan Mauldin <ryant.mauldin@gmail.com>
2022-10-28 18:33:31 -0700

    Projectiles on use forces and can now follow target

--- Assets/Scripts/Hazard/HazardProjectile.cs
+++ Assets/Scripts/Hazard/HazardProjectile.cs
@@ -6,10 +6,24 @@ public class HazardProjectile : MonoBehaviour
 {
     [SerializeField] bool rotate;
     [SerializeField] float degreesPerSecond;
+    [SerializeField] float maxSteeringForce;
+
+    Rigidbody2D m_RigidBody; 
+    SpriteRenderer spriteRenderer;
 
     private Queue<HazardCommand> commands;
     private HazardCommand currentCommand;
     private bool waiting;
+    private float spriteRadius;
+    private float commandStartTime;
+
+    void Start()
+    {
+        m_RigidBody = this.GetComponent<Rigidbody2D>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        spriteRadius = (spriteRenderer.bounds.center - spriteRenderer.bounds.size).magnitude;
+        this.waiting = false;
+    }
 
     private Vector2 CalculateVectorForce(Vector2 from, Vector2 to, float speed, float mass)
     {
@@ -33,7 +47,6 @@ public class HazardProjectile : MonoBehaviour
     
     private void HandleFire(FireCommand command)
     {
-        var m_RigidBody = this.GetComponent<Rigidbody2D>();
         var vectorForce = this.CalculateVectorForce(command.From, (Vector2)command.To, command.Speed,m_RigidBody.mass);
         m_RigidBody.AddForce(vectorForce);
         this.currentCommand = null;
@@ -41,20 +54,75 @@ public class HazardProjectile : MonoBehaviour
 
     private void HandleMove(MoveCommand command)
     {
-        var m_RigidBody = this.GetComponent<Rigidbody2D>();
 
-        var delta = Vector2.MoveTowards(m_RigidBody.position, command.To, command.Speed * Time.deltaTime);
-        m_RigidBody.MovePosition(delta);
-        if(Vector2.Distance(m_RigidBody.position, command.To) <= 0.01f)
-        {
+        if(Vector2.Distance(m_RigidBody.position, command.To) <= spriteRadius){
+            if(command.StopAtDestination)
+                m_RigidBody.velocity = Vector2.zero;
             this.currentCommand = null;
+            m_RigidBody.drag = 0;
+            return;
         }
+
+        if(Time.time - this.commandStartTime > command.TimeLimit)
+        {
+            Debug.Log("Time limit reached");
+            this.currentCommand = null;
+            m_RigidBody.drag = 0;
+            return;
+        }
+
+        var forceVector = CalculateVectorForce(m_RigidBody.position, command.To, command.Speed * Time.deltaTime, m_RigidBody.mass);
+
+        // slow arrival not working rn
+        // if(command.SlowArrival)
+        // {
+        //     // float distanceSquared = ((m_RigidBody.position - command.To).normalized * command.Speed).sqrMagnitude;
+        //     // float slowRadiusSquared = command.SlowArrivalRadius * command.SlowArrivalRadius;
+        //     float distanceSquared = Mathf.Pow(Vector2.Distance(m_RigidBody.position, command.To), 2);
+        //     float slowRadiusSquared = Mathf.Pow(command.SlowArrivalRadius, 2);
+        //     if (distanceSquared < slowRadiusSquared)
+        //     {
+        //         forceVector = forceVector * Mathf.Sqrt(distanceSquared / slowRadiusSquared) * Time.deltaTime / command.Speed;
+        //     }
+        // }
+
+        Vector2 steering = (forceVector - m_RigidBody.velocity) / Time.deltaTime;
+        Vector2 steerForce = m_RigidBody.mass * steering;
+        steerForce = Vector2.ClampMagnitude(forceVector, maxSteeringForce);
+        m_RigidBody.AddForce(steerForce);
+
+        // Seek
+        // ----------------------
+        // Vector2 desiredVel = (to - from).normalized * controller.MaxSpeed;
+        // Vector2 steering = (desiredVel - controller.Rigidbody2D.velocity) / Time.deltaTime;  // acceleration effectively
+        // Vector2 steerForce = controller.Rigidbody2D.mass * steering;
+        // steerForce = Vector2.ClampMagnitude(moveForce, maxSteeringForce);
+        // ----------------------
+
+
+        // ArrivalBehavior
+        // ----------------------
+        // public static Vector2 ArrivalBehavior(EnemyController controller, GameObject target, float slowRadius)
+        // ^^ HEADER
+        // Vector2 desiredVel = target.transform.position - controller.transform.position;
+        // float distanceSquared = desiredVel.sqrMagnitude;
+        // float slowRadiusSquared = slowRadius * slowRadius;
+
+        // if (distanceSquared < slowRadiusSquared)
+        // {
+        //             desiredVel = desiredVel.normalized * controller.MaxSpeed * Mathf.Sqrt(distanceSquared / slowRadiusSquared);
+        // }
+        // else // Outside the slowRadius
+        // {
+        //             desiredVel = desiredVel.normalized * controller.MaxSpeed;
+        // }
+
+        // Vector2 steering = desiredVel - controller.Rigidbody2D.velocity; 
+        // ----------------------
     }
 
     private void HandleDragChange(DragChangeCommand command)
     {
-        Debug.Log("Changing drag " + command.LinearDrag);
-        Rigidbody2D m_RigidBody = GetComponent<Rigidbody2D>();
         m_RigidBody.drag = command.LinearDrag;
         m_RigidBody.angularDrag = command.AngularDrag;
         this.currentCommand = null;
@@ -72,8 +140,10 @@ public class HazardProjectile : MonoBehaviour
     {
         if(this.currentCommand == null)
         {
-            if(this.commands.Count > 0)
+            if(this.commands.Count > 0){
                 this.currentCommand = this.commands.Dequeue();
+                this.commandStartTime = Time.time;
+            }
         }
 
         switch(this.currentCommand)
@@ -83,6 +153,7 @@ public class HazardProjectile : MonoBehaviour
             break;
 
             case MoveCommand m:
+            m_RigidBody.drag = 5;
             HandleMove(m);
             break;
 
@@ -98,11 +169,6 @@ public class HazardProjectile : MonoBehaviour
             break;
         }
     }
-
-    void Start()
-    {
-        this.waiting = false;
-    }
     void FixedUpdate()
     {
         HandleExecuteCommand();
@@ -114,7 +180,6 @@ public class HazardProjectile : MonoBehaviour
         {
             this.transform.Rotate(new Vector3(0, 0, degreesPerSecond) * Time.deltaTime);
         }
-        // HandleExecuteCommand();
     }
 
     void OnBecameInvisible()
