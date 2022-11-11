42cae5a69b7fca268cab703682c289c7ab5916f2
cjabs1997 <cjabs97@gmail.com>
2022-11-11 13:15:08 -0800

    Adjusted implementation to the simpler version
    I think we'll only care about horizontal distance in this case so that's all it adjust too. Left the fancier implementation in but commented it out for now. Adjust the size of the CameraBounds object to change camera size.

--- Assets/Scripts/Camera/CameraResizer.cs
+++ Assets/Scripts/Camera/CameraResizer.cs
@@ -6,11 +6,22 @@ public class CameraResizer : MonoBehaviour
 {
     [SerializeField] SpriteRenderer cameraBounds;
 
+    private void Awake()
+    {
+        cameraBounds.color = new Color(0,0, 0, 0);
+    }
+
     private void Start()
     {
-        float screenRatio = (float)Screen.width / (float)Screen.height;
-        float boundsRatio = cameraBounds.bounds.size.x / cameraBounds.bounds.size.y;
 
+        Camera.main.orthographicSize = cameraBounds.bounds.size.x * Screen.height / Screen.width * 0.5f;
+
+        // Fancier alternative the video had for making sure the bounds are set regardless of orientation or aspect ratio
+        //    This feel not needed to me but leaving here just in case
+        //float screenRatio = (float)Screen.width / (float)Screen.height;
+        //float boundsRatio = cameraBounds.bounds.size.x / cameraBounds.bounds.size.y;
+
+        /*
         if (screenRatio >= boundsRatio)
         {
             Camera.main.orthographicSize = cameraBounds.bounds.size.y / 2;
@@ -20,5 +31,6 @@ public class CameraResizer : MonoBehaviour
             float difference = boundsRatio / screenRatio;
             Camera.main.orthographicSize = cameraBounds.bounds.size.y / 2 * difference;
         }
+        */
     }
 }
